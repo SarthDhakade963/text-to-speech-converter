435794245792b5688fd4b7d048df02d231b9d482
Sarthak <sarthakdhakde@gmail.com>
2025-06-19 17:12:56 +0530

    implement pitch, volume and rate settings

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
+const pitchRange = document.querySelector("pitchRange");
+const volumeRange = document.querySelector("volumeRange");
+const rateRange = document.querySelector("rateRange");
+
+const pitchValue = document.querySelector("pitchValue");
+const volumeValue = document.querySelector("volumeValue");
+const rateValue = document.querySelector("rateValue");
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   if (!voices.length) return;
@@ -16,9 +24,30 @@ voiceSelect.addEventListener("change", () => {
   speech.voice = voices[voiceSelect.value];
 });
 
+// Update displayed values
+pitchRange.addEventListener("input", () => {
+  pitchValue.textContent = pitchRange.value;
+});
+
+rateRange.addEventListener("input", () => {
+  rateValue.textContent = rateRange.value;
+});
+
+volumeRange.addEventListener("input", () => {
+  volumeRange.textContent = volumeRange.value;
+});
+
 document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
+  const text = document.querySelector("textarea").value.trim();
+  if (!text) alert("Please enter some text");
+
+  speech.text = text;
   speech.voice = voices[voiceSelect.value];
-  window.speechSynthesis.cancel();
+  speech.pitch = parseFloat(pitchRange.value);
+  speech.rate = parseFloat(rateRange.value);
+  speech.volume = parseFloat(volumeRange.value);
+
+
+  window.speechSynthesis.cancel(); // block any ongoing voice 
   window.speechSynthesis.speak(speech);
 });
